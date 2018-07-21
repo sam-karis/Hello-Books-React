8847423773933a3a5929e1a11bb3124962212d32
sam-karis <samkaris75@gmail.com>
2018-07-21 23:55:05 +0300

    Add homepage component.

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: true
+    }
+  }
+  _handleClick = () => (this.setState({
+    loggedIn: !this.state.loggedIn
+  }));
+
+  render() {
+    return (
+      <div id='homecontent'>
+        
+          <section id="section"> Welcome to hello books</section>
+      </div>
+    );
+  }
+}
+
+export default Home;
