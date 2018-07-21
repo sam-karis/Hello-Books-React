6b351240718f494e71ca19343e091aa69ee3f2e6
sam-karis <samkaris75@gmail.com>
2018-07-21 23:50:43 +0300

    Add footer componennt.

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="signupFooter">
+                <br />
+                <div className="text-center text-muted">
+                    <small>
+                        <p>Developed by Sammy Kariuki &copy; Copyright
+                    <script>
+                                document.write(new Date().getFullYear())
+                    </script> Term & Condition Apply</p>
+                    </small>
+                </div>
+                <br />
+            </footer>
+        );
+    }
+}
