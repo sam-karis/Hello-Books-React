a2bc406c8cda4a0b6b8252db25b760020e612a2e
sam-karis <samkaris75@gmail.com>
2018-07-26 18:41:50 +0300

    [Feature #159300303] Add add book reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import auth from './AuthReducer';
 import { books, book } from './BooksReducer';
+import admin from './AdminReducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   auth,
+  admin,
   books,
   book
 });
