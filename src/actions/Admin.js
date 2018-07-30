bafdb2a0f32212e3b0865b012fb2bf73c96b6603
sam-karis <samkaris75@gmail.com>
2018-07-30 19:39:53 +0300

    [Feature #159376787]Add edit book action.

--- src/actions/Admin.js
+++ src/actions/Admin.js
@@ -4,30 +4,64 @@ import swal from 'sweetalert';
 import { api_url, request_header } from './../config';
 import { logout } from '../actions/Logout';
 
-export const addBook = (dispatch, data) => {
-  const add_book_url = `${api_url}books`;
-  axios
-    .post(
-      add_book_url,
-      {
-        title: data.title,
-        author: data.author,
-        edition: data.edition,
-        description: data.description
-      },
-      { headers: request_header(data.access_token) }
-    )
-    .then(res => {
-      const Message = res.data.Message;
-      dispatch({ type: 'ADD_BOOK_SUCCESS', data: { Message } });
-      swal(res.data.Message);
-      browserHistory.push('/books');
-    })
-    .catch(error => {
-      const Message = error.response.data.Message;
-      dispatch({ type: 'ADD_BOOK_FAIL', data: { Message } });
-      swal('NOT ACCEPTABLE', Message, 'error');
-    });
+export const addBook = data => {
+  return dispatch => {
+    const add_book_url = `${api_url}books`;
+    return axios
+      .post(
+        add_book_url,
+        {
+          title: data.title,
+          author: data.author,
+          edition: data.edition,
+          description: data.description
+        },
+        { headers: request_header(data.access_token) }
+      )
+      .then(res => {
+        const Message = res.data.Message;
+        dispatch({ type: 'ADD_BOOK_SUCCESS', data: { Message } });
+        swal(res.data.Message);
+        browserHistory.push('/books');
+      })
+      .catch(error => {
+        const Message = error.response.data.Message;
+        dispatch({ type: 'ADD_BOOK_FAIL', data: { Message } });
+        swal('NOT ACCEPTABLE', Message, 'error');
+      });
+  };
+};
+
+export const editBook = data => {
+  return dispatch => {
+    const edit_book_url = `${api_url}books/${data.book_id}`;
+    return axios
+      .put(
+        edit_book_url,
+        {
+          title: data.title,
+          author: data.author,
+          edition: data.edition,
+          description: data.description
+        },
+        { headers: request_header(data.access_token) }
+      )
+      .then(res => {
+        const Message = res.data.Message;
+        dispatch({ type: 'EDIT_BOOK_SUCCESS', data: { Message } });
+        swal(res.data.Message);
+        browserHistory.push(`/books/${data.book_id}`);
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          const Message = 'session expired login to continue';
+          swal(Message);
+          dispatch(
+            logout({ email: data.email, access_token: data.access_token })
+          );
+        }
+      });
+  };
 };
 
 export const deleteBook = data => {
